4118807d665c18611a5c79b042b7f711eed800ce
z01 <z01@z01>
2015-08-11 17:54:15 +0800

    another cost function which can extend to multiply classes

--- learn_theano/ocr_mlp.py
+++ learn_theano/ocr_mlp.py
@@ -4,6 +4,7 @@ import cv2
 import theano
 import theano.tensor as T
 
+#you may try several times to get a good model and the init 'cost' may be quite large, 78 .e.g
 class Layer(object):
     """
     a layer is a maxtrix with row = output of this layer and col = output of 
@@ -40,7 +41,8 @@ class MLP(object):
             x = layer.output(x)
         return x
     def squared_error(self,x,y):
-        return T.sum((self.output(x) - y) ** 2)
+         return T.sum((self.output(x) - y) ** 2)
+         return T.mean((self.output(x) - y) ** 2)
 
 def gradient_updates_momentum(cost, params, learning_rate, momentum):
     assert momentum < 1 and momentum >= 0
@@ -54,7 +56,7 @@ def gradient_updates_momentum(cost, params, learning_rate, momentum):
 class SAMPLE:
     def extract_feature(self, imgpath):
         img = cv2.imread(imgpath, 0)
-        stdw,stdh = (16,32)
+        stdw,stdh = (8,16)
         img = cv2.resize(img, (stdw,stdh))
         feat = np.zeros((1,stdw * stdh))
         k = 0
@@ -87,31 +89,43 @@ class SAMPLE:
             sample_list.extend(s)
         sample_num = len(sample_list)
         sample_dim = sample_list[0].shape[1]
+        label_num = sample_num
+        label_dim = len(set(label_list))
+
         targets = np.array(label_list)
         samples = np.zeros( (sample_num,sample_dim ) ) 
         for k in range(len(sample_list)):
             samples[k,:] = sample_list[k]
-        return (samples,targets)
+        targets2 = np.zeros((label_dim, label_num)) #the targets compatible with net output
+        for k in range(label_num):
+            l = targets[k]
+            targets2[l, k] = 1      
+        return (samples,targets, targets2)
+
 def calc_accuration(targets, outputs):
-    return np.mean((outputs > 0.5) == targets)
+#    return np.mean((outputs > 0.5) == targets)
     hit = 0
     for k in range(outputs.shape[1]):
         o = outputs[:,k]
-        o = o / o.max()
-        o = o >= 1
-        if o.sum() > 1:
+        t = targets[0,k]
+        num = 0
+        for i in o:
+            if np.abs(i - o.max()) < 0.001:
+                num += 1
+        if num > 1:
             continue
-        t = targets[k]
-        if o == t: #binary
-            hit += 1
-    return hit * 1. / targets.shape[0]
+        if np.abs(o[t] - o.max()) < 0.001:
+            hit += 1 
+
+    return hit * 1. / outputs.shape[1]
  
 def train_mlp(rootdir):
     spl = SAMPLE()
-    samples, targets = spl.load_all_samples(rootdir)
+    samples, targets, targets2 = spl.load_all_samples(rootdir)
     sample_num, sample_dim = samples.shape
     target_num = len(targets)
-    target_dim = len(set(targets)) - 1 # -1
+    target_dim = len(set(targets)) # -1 for binary-classes problem
+    targets = np.reshape(targets, (1, -1))
     assert sample_num == target_num
     layer_sizes = [sample_dim, 2 * sample_dim, target_dim]
     W_init = []
@@ -123,40 +137,41 @@ def train_mlp(rootdir):
         activations.append(T.nnet.sigmoid)
     mlp = MLP(W_init, b_init, activations)
     mlp_input = T.matrix('mlp_input')
-    mlp_target = T.vector('mlp_target')
+    mlp_target = T.matrix('mlp_target')
     learning_rate = 0.01
     momentum = 0.9
     cost = mlp.squared_error(mlp_input, mlp_target)
     train = theano.function([mlp_input,mlp_target], cost, updates=gradient_updates_momentum(cost, mlp.params, learning_rate, momentum))
     predict = theano.function([mlp_input],  mlp.output(mlp_input))
-
     
     iteration = 0
-    max_iteration = 2000
+    max_iteration = 512
     samplesT = np.transpose(samples) #W*x + b
     while iteration < max_iteration:
-        current_cost = train(samplesT, targets)
+        if 0:
+            for k in range(samplesT.shape[1]):
+                s = np.reshape(samplesT[:,k],(-1,1))
+                t = np.reshape(targets2[:,k],(-1,1))
+                current_cost = train(s,t)
+            
+        current_cost = train(samplesT,targets2)
         current_output = predict(samplesT)
+        print iteration, ' ', current_cost.mean(), ' ', calc_accuration(targets, current_output)
         iteration += 1
-        print 'iteration ', iteration, ' cost ', current_cost.mean(), ' accuration ', calc_accuration(targets, current_output)
-
     with open('model.ocr_mlp.txt', 'w') as f:
         pickle.dump(predict, f)
 
 def predict_mlp(rootdir):
     spl = SAMPLE()
-    samples, targets = spl.load_all_samples(rootdir)
+    samples, targets, targets2 = spl.load_all_samples(rootdir)
 
     with open('model.ocr_mlp.txt', 'r') as f:
         predict = pickle.load(f)
 
     samplesT = np.transpose(samples) #W*x + b
     output = predict(samplesT)
+    targets = np.reshape(targets,(1,-1))
     print 'accuration ', calc_accuration(targets, output)
-
-
-
-
 
 if __name__=='__main__':
     if len(sys.argv) >= 2:
