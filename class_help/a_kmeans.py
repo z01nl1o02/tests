fe146242a056a54727af491e337d9053274d3afc
z01 <z01@z01>
2015-10-14 16:31:13 +0800

    update kmeans modules for clarity

--- class_help/a_kmeans.py
+++ class_help/a_kmeans.py
@@ -5,15 +5,9 @@ from sklearn.decomposition import PCA
 import image_gabor_feature as igbf
 import image_lbp_feature as ilbpf
 
-def KMeans_A(rootdir, posdir,posnum,negnum_p,ft):
+def KMeans_A(rootdir,ft):
     pos = []
-    neg = [] 
-    pathpos = []
-    pathneg = []
-    folders = []
     imgspos = []
-    imgsneg = []
-    folders = [posdir] #only check the pointed folder
     if 0 == cmp(ft, 'lbp'):
         print "ft : LBP"
         gbf=ilbpf.LBP_FEAT()
@@ -23,30 +17,14 @@ def KMeans_A(rootdir, posdir,posnum,negnum_p,ft):
     else:
         print 'unknown ft'
         return 
-    for folder in folders:
-        fname = os.path.join(rootdir, folder)
-        if 0 == cmp(folder, posdir):
-            fvs,imgs = gbf.gen_folder(fname, posnum)
-            if fvs is None:
-                print 'pos None ',fname
-                continue
-            pos.extend(fvs)
-            imgspos.extend(imgs)
-            pathpos.extend([folder for k in range(len(fvs))])
-        else:
-            fvs,imgs = gbf.gen_folder(fname, negnum_p)
-            if fvs is None:
-                print 'neg None ', fname
-                continue
-            neg.extend(fvs)
-            imgsneg.extend(imgs)
-            pathneg.extend([folder for k in range(len(fvs))])
-    label0 = [0 for k in range(len(pos))]
-    label1 = [1 for k in range(len(neg))]
-    samples = np.array(pos + neg)
-    labels = np.array(label0 + label1)
-    paths = pathpos + pathneg
-    imgs = imgspos + imgsneg
+    fvs,imgs = gbf.gen_folder(rootdir, 5000)
+    if fvs is None:
+        print 'JPG None ',rootdir
+        return
+    pos.extend(fvs)
+    imgspos.extend(imgs)
+    samples = np.array(pos)
+    imgs = imgspos
     com_num = np.minimum(300, samples.shape[0] - 10)
     clf = PCA(com_num)
     print 'before pca : ', samples.shape
@@ -78,15 +56,12 @@ def KMeans_A(rootdir, posdir,posnum,negnum_p,ft):
     return 
 
 if __name__=="__main__":
-    with open('config.txt','r') as f:
-        rootdir = f.readline().strip()
-        posdir = f.readline().strip()
-        posnum = np.int64(f.readline().strip())
-        negnum_p = np.int64(f.readline().strip())
-        ft = f.readline().strip()
-    posnum = 5000
-    negnum_p = 5000
-    KMeans_A(rootdir, posdir,posnum, negnum_p,ft)
+    if len(sys.argv) == 3:
+        ft = sys.argv[1]
+        rootdir = sys.argv[2]
+        KMeans_A(rootdir,ft)
+    else:
+        print 'unknown option'
 
 
 
