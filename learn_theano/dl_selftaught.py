565cf141b99fadd33d6ef44f2a8bfd501133baef
z01 <z01@z01>
2016-01-06 22:42:27 +0800

    update

--- learn_theano/dl_selftaught.py
+++ learn_theano/dl_selftaught.py
@@ -5,8 +5,8 @@ import theano.tensor as T
 
 class PARAM(object):
     def __init__(self):
-        self.imgw = 16
-        self.imgh = 32
+        self.imgw = 10
+        self.imgh = 10
 
 class LAYER(object):
     def __init__(self, init_W, init_b, tranfunc):
@@ -43,10 +43,12 @@ class MLP(object):
     def calc_error(self,x,y):
         py = self.output(x)
         error = T.sum((py - y) ** 2)
+        """
         wc = 0
         for param in self.params:
             wc += np.sum(param.get_value() ** 2)
         error =error * 0.5 + wc * 0.5
+        """
         return error
 
     def gen_update_pairs(self, cost, learn_ratio, moment):
@@ -73,8 +75,8 @@ def gen_initial_param(layer_sizes):
     return (Ws, bs, tfs)
 
 def create_config_net(layer_sizes):
-    learn_ratio = 0.01
-    moment = 0.9
+    learn_ratio = 0.4
+    moment = 0.7
     net_in = T.matrix('net_in')
     net_out = T.matrix('net_out')
     
@@ -98,6 +100,8 @@ class DATA(object):
         img = cv2.resize(img, (stdw,stdh))
         img = np.reshape(img, (1,-1))
         img = img / 255.0
+        norm = img * img
+        img = img / norm.sum() #norm is not so important although in theory it is key
         return img
     
     def load_label_sample(self, rootdir, label):
@@ -126,7 +130,7 @@ class DATA(object):
     def load(self, rootdir):
         labels = []
         samples = []
-        labellist = range(0,5,1)
+        labellist = range(0,1,1)
         for label in labellist:
             s,l = self.load_label_sample(rootdir, label)
             samples.extend(s)
@@ -160,7 +164,8 @@ def gen_train_samples(samples, labels, labellist):
 #    trainset = whiten_data(trainset)
     trainlabel = trainset #self-map
 
-    trainset = whiten_data(trainset)
+    
+    #trainset = whiten_data(trainset)
 
     return (trainset, trainlabel)
 
@@ -187,7 +192,7 @@ def calc_false_alarm(target,output):
     return 1 - hit * 1. / target.shape[1]
 
 def draw_prd(prd):
-    for k in range(5):
+    for k in range(0,100,10):
         img = np.zeros((PARAM().imgh,PARAM().imgw))
         for y in range(PARAM().imgh):
             for x in range(PARAM().imgw):
@@ -201,13 +206,14 @@ def draw_weight(net):
     W = net.layers[0].W.get_value()
     for k in range(W.shape[0]):
         img = np.reshape(W[k,:],(PARAM().imgh,PARAM().imgw))
-   #     norm = np.sqrt(np.sum(img ** 2))
-   #     img = img / norm
+        norm = np.sqrt(np.sum(img ** 2))
+        img = img / norm
         if k < 3:
             print ' ', img.min(), ' ', img.max(), ' ', W.shape
 #        img = np.abs(img)
         img = (img - img.min()) * 255.0 / (img.max() - img.min())
         img = np.uint8(img)
+        img = cv2.resize(img, (PARAM().imgh * 5,PARAM().imgw * 5) )
         cv2.imwrite('d:/tmp/ALPR/ocr'+str(k)+'.jpg', img) 
 
 
@@ -239,12 +245,14 @@ def do_train(rootdir):
             all_cost += cur_cost.sum()
             all_num += batchsize
             
-        if iterator % 5 == 0:
+        if iterator % 50 == 0:
             prd = predict(trainset) 
             fa = calc_false_alarm(trainlabel, prd)
             print iterator, ' ', all_cost*1./all_num, ' ', fa
             draw_weight(net)
             draw_prd(prd)
+            with open('dl.dat', 'wb') as f:
+                pickle.dump((train,predict,net), f)
 
 if __name__=="__main__":
     if len(sys.argv) == 3 and 0 == cmp(sys.argv[1], '-train'):
