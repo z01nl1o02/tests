afd6f4afacee2b99177c28ee86f2b9aa06355e8f
ss <ss@01>
2018-04-28 21:31:28 +0800

    net symbol

---
+++ mxnet/cifar/cifar10/symbol/resnet18.py
@@ -0,0 +1,46 @@
+import os,sys,pdb
+import numpy as np
+import mxnet as mx
+from mxnet import gluon
+from mxnet.gluon import nn
+from mxnet import nd
+class Residual(nn.HybridBlock):
+    def __init__(self, channels, same_shape=True, **kwargs):
+        super(Residual, self).__init__(**kwargs)
+        self.same_shape = same_shape
+        with self.name_scope():
+            strides = 1 if same_shape else 2
+            self.conv1 = nn.Conv2D(channels, kernel_size=3, padding=1,
+                                  strides=strides)
+            self.bn1 = nn.BatchNorm()
+            self.conv2 = nn.Conv2D(channels, kernel_size=3, padding=1)
+            self.bn2 = nn.BatchNorm()
+            if not same_shape:
+                self.conv3 = nn.Conv2D(channels, kernel_size=1,
+                                      strides=strides)
+
+    def hybrid_forward(self, F, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if not self.same_shape:
+            x = self.conv3(x)
+        return F.relu(out + x)
+
+def get_symbol(num_classes,ctx,**kwargs):
+    net = nn.HybridSequential()
+    with net.name_scope():
+        net.add(
+            nn.BatchNorm(),
+            nn.Conv2D(64, kernel_size=3, strides=1),
+            nn.MaxPool2D(pool_size=3, strides=2),
+            Residual(64),
+            Residual(64),
+            Residual(128, same_shape=False),
+            Residual(128),
+            Residual(256, same_shape=False),
+            Residual(256),
+            nn.GlobalAvgPool2D(),
+            nn.Dense(num_classes)
+        )
+    net.initialize(ctx = ctx)
+    return net
