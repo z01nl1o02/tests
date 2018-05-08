6ff3195ed78b5c08292c48c97a6ac839256c7903
ss <ss@01>
2018-05-08 23:40:07 +0800

    python to parse/show caffe log (shell is excluded)

---
+++ caffe-log/show_log.py
@@ -0,0 +1,72 @@
+import os,sys,pdb
+from matplotlib import pyplot as plt
+from collections import defaultdict
+import numpy as np
+import argparse
+
+
+def show_train(infile):
+    stat = defaultdict(list)
+    with open(infile, 'rb') as f:
+        line = f.readline()
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            NumIters,Seconds,LearningRate,loss = [np.float32(d) for d in line.split(',')]
+            stat['NumIters'].append( NumIters )
+            stat['Seconds'].append( Seconds )
+            stat['LearningRate'].append( LearningRate )
+            stat['loss'].append(loss)
+    return stat
+    
+
+def show_test(infile):
+    stat = defaultdict(list)
+    with open(infile, 'rb') as f:
+        line = f.readline()
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            NumIters,Seconds,LearningRate,accuracy,loss = [np.float32(d) for d in line.split(',')]
+            stat['NumIters'].append( NumIters )
+            stat['Seconds'].append( Seconds )
+            stat['LearningRate'].append( LearningRate )
+            stat['accuracy'].append(accuracy)
+            stat['loss'].append(loss)
+    return stat
+    
+def show_log(logpath):
+    trainstat = show_train( logpath + '.train')
+    teststat = show_test( logpath + '.test')
+    plt.figure()
+    plt.subplot(311)
+    plt.plot(trainstat['NumIters'], trainstat['loss'], label='train loss')
+    plt.legend()
+    plt.subplot(312)
+    plt.plot(teststat['NumIters'], teststat['loss'], label='test loss')
+    plt.legend()
+    plt.subplot(313)
+    plt.plot(teststat['NumIters'], teststat['accuracy'], label='test accuracy')
+    plt.legend()
+    plt.show()
+    
+if __name__=="__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument('logpath',help='path to log file')
+    ap.add_argument('-tmpdir',help='temp dir',default = 'temp')
+    args = ap.parse_args()
+    try:
+        os.makedirs(args.tmpdir)
+    except Exception,e:
+        pass
+    cmd = 'python parse_log.py "%s" "%s"'%(args.logpath, args.tmpdir)
+    os.system(cmd)
+    show_log( os.path.join(args.tmpdir, args.logpath.split(os.sep)[-1]) )
+
+        
+            
+            
+
+
