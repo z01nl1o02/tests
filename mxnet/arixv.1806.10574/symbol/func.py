fix memory invalid error. wait_to_read() to handle lazy computation

mxnet/arixv.1806.10574/symbol/func.py
@@ -1,6 +1,7 @@
 import os,sys,pdb
 import ctypes
 from mxnet.base import _LIB
+import mxnet as mx
 
 class CHLEPDLL:
     def __init__(self):
@@ -10,6 +11,8 @@ class CHLEPDLL:
 theLIB = CHLEPDLL()
 
 def get_pointer(v):
+    if isinstance(v, mx.ndarray.NDArray):
+        v.wait_to_read() #lazy computation of mxnet
     ptr = ctypes.c_void_p()
     _LIB.MXNDArrayGetData(v.handle, ctypes.byref(ptr))
     return ptr
@@ -20,12 +23,23 @@ class CHLEPFUNC:
         self.func = getattr(theLIB.lib, self.name)
         return
     def __call__(self, *args, **kwargs):
-        dev_id, in_mat, out_mat = args
+        in_mat_xpu = args[0]
+        if in_mat_xpu.context == mx.cpu():
+            in_mat = in_mat_xpu
+        else:
+            in_mat = in_mat_xpu.as_in_context(mx.cpu())
         channels, height, width = in_mat.shape
         in_ptr = get_pointer(in_mat)
+        out_height = (height - 2) * (width - 2)
+        out_width = 3*3*channels
+        out_mat = nd.zeros((out_height, out_width), ctx = mx.cpu(), dtype=np.float32)
         out_ptr = get_pointer(out_mat)
-        self.func(dev_id, in_ptr, channels, height, width, out_ptr)
-        return
+        self.func(0, in_ptr, channels, height, width, out_ptr)
+        if in_mat_xpu.context == mx.cpu():
+            out_mat_xpu = out_mat
+        else:
+            out_mat_xpu = out_mat.as_in_context(mx.gpu())
+        return out_mat_xpu
 
 patch2col = CHLEPFUNC('patch2col')
 
@@ -34,14 +48,11 @@ if 0: # testing
     from mxnet import ndarray as nd
     import numpy as np
     a = np.random.random((4,8,8))
-    a = nd.array(a, ctx = mx.cpu())
-    b = nd.zeros((6*6,4*3*3),ctx=a.context,dtype=np.float32)
-    patch2col(0,a,b)
-    print a
-    print b
+    a = nd.array(a, ctx = mx.gpu())
+    b = patch2col(a)
 
-
-    c = nd.zeros((6,6),ctx=a.context,dtype=np.float32)
-
-
+    print '====================a====================='
+    print a.asnumpy()
+    print '====================b====================='
+    print b.asnumpy()
 